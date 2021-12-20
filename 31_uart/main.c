f0792ad1cdd037426138ecb0690b1463f52ef398
Artem Derkach <artem.derkach.gs@gmail.com>
2021-12-20 22:40:42 +0200

    working version of uart

--- 31_uart/main.c
+++ 31_uart/main.c
@@ -5,32 +5,33 @@
 int main(void) {
     // GPIO
     // Enable clock access
-    RCC->APB1ENR |= RCC_AHB1ENR_GPIOAEN;
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
 
-    // PA2 Alternate function mode (10)
-    GPIOA->MODER |=  GPIO_MODER_MODER2_1;
-    GPIOA->MODER &= ~GPIO_MODER_MODER2_0;
+    // PA2 Alternate function mode ([5:4] bit is [1:0])
+    GPIOA->MODER &= ~GPIO_MODER_MODER2_0; // 4-th bit
+    GPIOA->MODER |=  GPIO_MODER_MODER2_1; // 5-th bit
 
     // Select alternate function AF7 (0111) for USART2
     GPIOA->AFR[0] |=  GPIO_AFRL_AFRL2_0;
     GPIOA->AFR[0] |=  GPIO_AFRL_AFRL2_1;
     GPIOA->AFR[0] |=  GPIO_AFRL_AFRL2_2;
-    GPIOA->AFR[0] &= &GPIO_AFRL_AFRL2_3;
+    GPIOA->AFR[0] &= ~GPIO_AFRL_AFRL2_3;
 
     // UART
     // Enable clock access
     RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
 
     // Configure baud rate
-
+    USART2->BRR = ((16000000 + (115200/2U))/115200);
 
     // Configure transfer direction
-    USART2->CR1 |= USART_CR1_TE;
+    USART2->CR1 = USART_CR1_TE;
 
     // Enable UART module
     USART2->CR1 |= USART_CR1_UE;
 
     while (1) {
-
+        while(!(USART2->SR & USART_SR_TXE)){}
+        USART2->DR	=  ('E' & 0xFF);
     }
 }
